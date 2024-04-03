7d6d6f6b5263a0d763b89f2d5553cd242ef3cbf0
Rosamon <klimroman2001@mail.ru>
2024-04-03 21:15:03 +0300

    Script is done

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
-# open th file "/proc/sys/fs/file-nr" in read mode
+#!/usr/bin/python3
+
+# plan:
+# open the file "/proc/sys/fs/file-nr" in read mode
 # first value is the number of open file descriptions (means files)
-# 
+# so read file and get first value
+
+with open('/proc/sys/fs/file-nr', 'r') as file:
+    number = file.read().rstrip().split()[0]
+
+print(number)
